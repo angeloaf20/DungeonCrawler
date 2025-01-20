55dc036fd286d60430d54abb880dbfb5de254a0f
Angelo <107896647+angeloaf20@users.noreply.github.com>
2025-01-20 07:18:49 +0000

    clear color and load glad

--- DungeonCrawler/DungeonCrawler.cpp
+++ DungeonCrawler/DungeonCrawler.cpp
@@ -1,4 +1,5 @@
-﻿#include <glad/glad.h>
+﻿#include <print>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 int main(void)
@@ -9,6 +10,10 @@ int main(void)
     if (!glfwInit())
         return -1;
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
@@ -17,13 +22,22 @@ int main(void)
         return -1;
     }
 
-
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::println("Failed to initialize GLAD");
+        return -1;
+    }
+
+    glViewport(0, 0, 640, 480);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.1f, 0.8f, 0.8f, 1.0f);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
